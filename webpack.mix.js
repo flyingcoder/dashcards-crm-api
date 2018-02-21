6c5fe7cda9521e9480c1e27a2419ee1ab0c90d0e
Klent Gary <klentgarymaligon@gmail.com>
2018-02-22 04:10:49 +0800

    Initial Transfer New Design

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js('resources/assets/js/buzzooka.js', 'public/js')
+   .sass('resources/assets/sass/buzzooka/buzzooka.scss', 'public/css')
+   .copyDirectory('resources/assets/img', 'public/img');
