49d2745b1c9d1965969bcab233f5fc28b46309b7
Klent Gary <klentgarymaligon@gmail.com>
2018-03-05 10:08:14 +0800

    Added Fixed Add Milestone Hover

--- resources/assets/js/components/projects/project-hq/index.vue
+++ resources/assets/js/components/projects/project-hq/index.vue
@@ -2,6 +2,7 @@
     <div>
         <hq-menu></hq-menu>
 
+        <milestones></milestones>
         <!-- <members> </members> -->
         <!-- <profile></profile> -->
     </div>
@@ -11,15 +12,16 @@
     import HQMenu from './hq-menu.vue';
     import PageHeader from '../../page-header.vue';    
     import BoxOption from '../../box-option.vue';
+    import Milestones from './milestones/index.vue';
     import Members from './members/index.vue';
     import Profile from '../../teams/profile/index.vue';
-   
 
     export default {
         components: {
           'hq-menu': HQMenu,
           'page-header': PageHeader,
           'box-option': BoxOption,
+          'milestones': Milestones,
           'members': Members,
           'profile': Profile,
         }
