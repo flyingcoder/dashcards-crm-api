ae89ec2cd604452ecdfe51892c62eb11fe9c071c
Klent Gary <klentgarymaligon@gmail.com>
2018-03-08 05:59:09 +0800

    Updated Dashboard Boxes Components

---
+++ resources/assets/js/components/dashboard/calendar/calendar.vue
@@ -0,0 +1,12 @@
+<template>
+     <div class="box-content db-calendar">
+        <div class="row">
+            <div class="col-md-7">
+            LEFT
+            </div>
+            <div class="col-md-5">
+            Right
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/assets/js/components/dashboard/client/client.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="box-content db-client">
+        <div class="row">
+            <div class="col-md-4">
+                <span class="name"> Total Sales </span> <br>
+                <span class="number"> 3491 </span>
+            </div>  
+            <div class="col-md-4">
+                <span class="name"> New Orders </span> <br>
+                <span class="number"> 721 </span>
+            </div>  
+            <div class="col-md-4">
+                <span class="name"> Total Earnings </span> <br>
+                <span class="number"> $8103 </span>
+            </div>  
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="clients-chart">
+                    <img src="img/temporary/clients.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
