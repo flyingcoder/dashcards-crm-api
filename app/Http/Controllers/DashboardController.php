23ccd1e89a278dc71379a45a5df5f13cc124f775
Alvin Pacot <apacot@MacBooks-MacBook-Pro.local>
2018-03-19 04:28:36 +0800

    Dashboard item and dashboard settings

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,27 @@ use Illuminate\Http\Request;
 
 use App\Dashboard;
 
+use App\Dashitem;
+
 class DashboardController extends Controller
 {
+    public function addDashitems()
+    {
+        $company = auth()->user()->company();
+
+        $defaultDash = $company->dashboards()->first();
+
+        request()->validate([
+            'dashitem_id' => 'required|array'
+        ]);
+        
+        foreach (request()->dashitem_id as $k => $id) {
+            $defaultDash->dashitems()->attach($id, ['order' => $k+1]);
+        }
+
+        return $defaultDash->dashitems;
+    }
+
     public function counts() {
 
     	$company = auth()->user()->company();
@@ -31,6 +50,24 @@ class DashboardController extends Controller
 	        	'outbound' => 0 //replied questionaires
     		];
     	}
+    }
+
+    public function hideAllDashitem()
+    {
+        $company = auth()->user()->company();
+
+        $defaultDash = $company->dashboards()->first();
+
+        return $defaultDash->dashitems()->detach();
+    }
+
+    public function hideDashitem($id)
+    {
+        $company = auth()->user()->company();
+
+        $defaultDash = $company->dashboards()->first();
+
+        return $defaultDash->dashitems()->detach($id);
     }
 
     public function dashitems($id)
--- tests/Feature/DashitemTest.php
+++ tests/Feature/DashitemTest.php
@@ -14,6 +14,39 @@ class DashitemTest extends TestCase
      *
      * @return void
      */
+    public function testAddDashitem()
+    {
+        $user = User::find(1);
+
+        $data = [
+            'dashitem_id' => [1,2,3]
+        ];
+
+        $response = $this->actingAs($user, 'api')
+                         ->withHeaders(['HTTP_X-Requested-With' => 'XMLHttpRequest'])
+                         ->post('api/dashboard/default/dashitems', $data);
+
+        dd($response->content());
+        $response->assertStatus(200);
+    }
+
+    public function testGetDashitems()
+    {
+        $user = User::find(1);
+
+        $response = $this->actingAs($user, 'api')
+                         ->withHeaders(['HTTP_X-Requested-With' => 'XMLHttpRequest'])
+                         ->get('api/dashitems');
+
+        //dd($response->content());
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
     public function testDashboard()
     {
         $user = User::find(1);
@@ -57,7 +90,7 @@ class DashitemTest extends TestCase
 
     	$response = $this->actingAs($user, 'api')
     					 ->withHeaders(['HTTP_X-Requested-With' => 'XMLHttpRequest'])
-    					 ->put('api/dashitem/1/order', $data);
+    					 ->put('api/dashitems/1/order', $data);
 
     	//dd($response->content());
     	$response->assertStatus(200);
