d34aadf7db210552f17d54a5fbd6395fed702308
Klent Gary <klentgarymaligon@gmail.com>
2018-03-21 10:20:59 +0800

    Added View Selected Task

--- resources/assets/js/components/dashboard/tasks/tasks.vue
+++ resources/assets/js/components/dashboard/tasks/tasks.vue
@@ -54,7 +54,32 @@
         },
     data() {
       return {
-
+        pickerOptions1: {
+            disabledDate(time) {
+                return time.getTime() > Date.now();
+            },
+            shortcuts: [{
+                text: 'Today',
+                onClick(picker) {
+                picker.$emit('pick', new Date());
+                }
+            }, {
+                text: 'Yesterday',
+                onClick(picker) {
+                const date = new Date();
+                date.setTime(date.getTime() - 3600 * 1000 * 24);
+                picker.$emit('pick', date);
+                }
+            }, {
+                text: 'A week ago',
+                onClick(picker) {
+                const date = new Date();
+                date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                picker.$emit('pick', date);
+                }
+            }]
+        },
+        value1: 'March 16, 2018',
       };
     },
     methods: {
