f7c0a9c969a93487bb65c5f0190c5934f344f74e
Klent Gary <klentgarymaligon@gmail.com>
2018-03-22 17:24:12 +0800

    Added Remaining Settings Files

--- resources/assets/js/components/settings/Index.vue
+++ resources/assets/js/components/settings/Index.vue
@@ -1,7 +1,6 @@
 <template>
     <section class="content settings">
         <settings-menu></settings-menu>
-        <PaymentMethods></PaymentMethods>
         <!-- <section>
             <transition name="fade" mode="out-in">
                 <router-view></router-view>
@@ -12,12 +11,10 @@
 
 <script>
     import SettingsMenu from './SettingsMenu.vue';
-    import PaymentMethods from './PaymentMethods.vue';
 
     export default {
         components: {
           'settings-menu': SettingsMenu,
-          'PaymentMethods': PaymentMethods,
       },
       props: ['asset'],
       
