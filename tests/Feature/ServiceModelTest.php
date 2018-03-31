bca238dbe437aaf926d952393895a6aa0b2ce539
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-03-31 22:57:21 +0800

    Fix services data

--- tests/Feature/ServiceModelTest.php
+++ tests/Feature/ServiceModelTest.php
@@ -34,6 +34,7 @@ class ServiceModelTest extends TestCase
                          ->withHeaders(['HTTP_X-Requested-With' => 'XMLHttpRequest'])
                          ->get('api/services');
         
+        dd($response->content());
         $response->assertStatus(200);
     }
 
