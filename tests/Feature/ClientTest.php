bb3522dbc6a7d0dd11c224d4b11ec6beaddb845f
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-03-31 22:38:51 +0800

    Fix clients error unknown order

---
+++ tests/Feature/ClientTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ClientTest extends TestCase
+{
+    public function testClients()
+    {
+        $user = User::find(1);
+
+        $response = $this->actingAs($user, 'api')
+                         ->withHeaders(['HTTP_X-Requested-With' => 'XMLHttpRequest'])
+                         ->get('api/clients');
+
+        dd($response->content());
+        $response->assertStatus(200);
+    }
+}
