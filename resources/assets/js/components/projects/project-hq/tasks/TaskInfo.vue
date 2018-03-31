49a32baf5abe53f538b4f235c0cfdef6dd942b8d
Klent Gary <klentgarymaligon@gmail.com>
2018-04-01 05:35:51 +0800

    Updated Timer Button CSS

--- resources/assets/js/components/projects/project-hq/tasks/TaskInfo.vue
+++ resources/assets/js/components/projects/project-hq/tasks/TaskInfo.vue
@@ -52,13 +52,27 @@
                             <button> Task Completed </button>
                         </li>
                         <li>
-                            <button> 
+                            <button class="timer-btn"> 
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                                     width="10px" height="12px">
                                     <path fill-rule="evenodd"  fill="rgb(255, 255, 255)"
                                     d="M0.569,11.942 C0.255,11.942 0.000,11.690 0.000,11.379 L0.000,1.512 C0.000,1.201 0.255,0.949 0.569,0.949 L9.810,6.023 C9.810,6.023 10.237,6.446 9.810,6.868 C9.384,7.291 0.569,11.942 0.569,11.942 Z"/>
                                 </svg>  
                             </button>
+                            <div class="hrs">
+                                3
+                                <small> HR </small>
+                            </div>
+                            <div class="divider"></div>
+                            <div class="mins">
+                                45
+                                <small> M </small>
+                            </div>
+                            <div class="divider"></div>
+                            <div class="secs">
+                                10
+                                <small> S </small>
+                            </div>
                         </li>
                     </ul>
                 </div>
