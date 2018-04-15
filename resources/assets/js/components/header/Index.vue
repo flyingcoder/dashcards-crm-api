74224022defd1c372a91f57f1d4252a23a2144de
Klent Gary <klentgarymaligon@gmail.com>
2018-04-16 01:18:34 +0800

    Fix Collapse Sidebar Bug on Header Menu

--- resources/assets/js/components/header/Index.vue
+++ resources/assets/js/components/header/Index.vue
@@ -27,38 +27,30 @@
               </button>
             </div>
         <div class="buzz-head">
-            <section>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="buzz-search">
-                        <form class="form-inline navbar-form pull-left">
-                            <div class="input-group search">
-                            <span class="input-group-btn">
-                                <button class="btn" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="26px" height="25px">
-                                <path fill-rule="evenodd"  fill="rgb(218, 225, 231)"
-                                    d="M24.643,22.771 L18.474,16.582 C20.060,14.764 20.929,12.476 20.929,10.094 C20.929,4.528 16.234,0.000 10.463,0.000 C4.693,0.000 -0.002,4.528 -0.002,10.094 C-0.002,15.659 4.693,20.187 10.463,20.187 C12.630,20.187 14.694,19.557 16.459,18.361 L22.675,24.596 C22.935,24.856 23.285,25.000 23.659,25.000 C24.014,25.000 24.350,24.869 24.605,24.632 C25.148,24.129 25.165,23.294 24.643,22.771 ZM10.463,2.633 C14.729,2.633 18.199,5.980 18.199,10.094 C18.199,14.207 14.729,17.554 10.463,17.554 C6.198,17.554 2.728,14.207 2.728,10.094 C2.728,5.980 6.198,2.633 10.463,2.633 Z"/>
-                                </svg>
-                                </button>
-                            </span>
-                            <input class="form-control" type="text" placeholder="Search">
-                            </div>
-                        </form>
-                        </div>
+            <div class="buzz-search">
+                <form class="form-inline navbar-form pull-left">
+                    <div class="input-group search">
+                    <span class="input-group-btn">
+                        <button class="btn" type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="26px" height="25px">
+                        <path fill-rule="evenodd"  fill="rgb(218, 225, 231)"
+                            d="M24.643,22.771 L18.474,16.582 C20.060,14.764 20.929,12.476 20.929,10.094 C20.929,4.528 16.234,0.000 10.463,0.000 C4.693,0.000 -0.002,4.528 -0.002,10.094 C-0.002,15.659 4.693,20.187 10.463,20.187 C12.630,20.187 14.694,19.557 16.459,18.361 L22.675,24.596 C22.935,24.856 23.285,25.000 23.659,25.000 C24.014,25.000 24.350,24.869 24.605,24.632 C25.148,24.129 25.165,23.294 24.643,22.771 ZM10.463,2.633 C14.729,2.633 18.199,5.980 18.199,10.094 C18.199,14.207 14.729,17.554 10.463,17.554 C6.198,17.554 2.728,14.207 2.728,10.094 C2.728,5.980 6.198,2.633 10.463,2.633 Z"/>
+                        </svg>
+                        </button>
+                    </span>
+                    <input class="form-control" type="text" placeholder="Search">
                     </div>
-                    <div class="col-md-6">
-                        <div class="buzz-nav-menu pull-right">
-                        <ul class="buzz-nav">
-                            <templates></templates>
-                            <email></email>
-                            <notifications></notifications>
-                            <chat></chat>
-                            <user></user>
-                        </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                </form>
+            </div>
+            <div class="buzz-nav-menu">
+                <ul class="buzz-nav">
+                    <templates></templates>
+                    <email></email>
+                    <notifications></notifications>
+                    <chat></chat>
+                    <user></user>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
--- resources/assets/js/components/header/User.vue
+++ resources/assets/js/components/header/User.vue
@@ -1,24 +1,19 @@
 <template>
     <li>
-        <a href="#">
-            <img src="/img/temporary/user.png" id="user-image">
-            <el-dropdown trigger="click" placement="bottom" class="member-option">
-                <el-button size="small" class="el-dropdown-link"> 
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40px" height="17px">
-                        <path fill-rule="evenodd"  fill="rgb(103, 116, 130)"
-                            d="M39.599,0.335 C39.061,-0.103 38.187,-0.103 37.648,0.335 L20.497,14.318 L3.311,0.335 C2.773,-0.103 1.899,-0.103 1.361,0.335 C0.823,0.773 0.823,1.484 1.361,1.922 L19.488,16.671 C19.757,16.890 20.093,17.000 20.463,17.000 C20.799,17.000 21.169,16.890 21.439,16.671 L39.565,1.922 C40.137,1.484 40.137,0.773 39.599,0.335 Z"/>
-                    </svg>
-                </el-button>
-                <el-dropdown-menu slot="dropdown" class="header-dropdown header-notifications buzz-scrollbar buzz-scroll">
-                    <el-dropdown-item>
-                        <div style="text-align:center; padding:5px">
-                            <a href="#">
-                                <h3>Account</h3> 
-                            </a>
-                        </div>
-                    </el-dropdown-item>
-                </el-dropdown-menu>
-            </el-dropdown>
-        </a>
+        <el-dropdown trigger="click" placement="bottom" class="member-option">
+            <el-button size="small" class="el-dropdown-link"> 
+                <img src="/img/temporary/user.png" id="user-image">
+                <img src="/img/icons/header/arrowdown.svg">
+            </el-button>
+            <el-dropdown-menu slot="dropdown" class="header-dropdown header-notifications buzz-scrollbar buzz-scroll">
+                <el-dropdown-item>
+                    <div style="text-align:center; padding:5px">
+                        <a href="#">
+                            <h3>Account</h3> 
+                        </a>
+                    </div>
+                </el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
     </li>
 </template>
