38feb2008331c87171e09cfe99b96b2abfb763c3
dustin fabre <dustinfabre.hbs@gmail.com>
2018-04-20 22:23:39 +0800

    removed files to refresh names

--- resources/assets/js/components/projects/project-hq/tasks/VideoTask.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="">
-        <div>
-            <img src="/img/temporary/construction.jpg" alt="icon" width="560px">
-        </div>
-        <h3 class="text-center"> Coming Soon!!</h3>
-    </div>
-</template>
