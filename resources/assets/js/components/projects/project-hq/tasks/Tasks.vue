6cf76d7feaa67f7304f6f4f8a8dff7bb6cc94d84
Klent Gary <klentgarymaligon@gmail.com>
2018-04-24 12:51:55 +0800

    Updated View More Placing

--- resources/assets/js/components/projects/project-hq/tasks/Tasks.vue
+++ resources/assets/js/components/projects/project-hq/tasks/Tasks.vue
@@ -76,33 +76,6 @@
                                 <div class="progress" :class="t.status"> </div>
                                 </td>
                             </tr>
-                            <tr  v-for="t in filteredTasks" :key="t.id" @click="clickTask(t)">
-                                <td> 
-                                    <div class="hover-display"> 
-                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                        width="18px" height="35px">
-                                        <path fill-rule="evenodd"  fill="rgb(218, 225, 231)"
-                                        d="M15.000,21.000 C13.343,21.000 12.000,19.657 12.000,18.000 C12.000,16.343 13.343,15.000 15.000,15.000 C16.657,15.000 18.000,16.343 18.000,18.000 C18.000,19.657 16.657,21.000 15.000,21.000 ZM15.000,6.000 C13.343,6.000 12.000,4.657 12.000,3.000 C12.000,1.343 13.343,-0.000 15.000,-0.000 C16.657,-0.000 18.000,1.343 18.000,3.000 C18.000,4.657 16.657,6.000 15.000,6.000 ZM3.000,35.000 C1.343,35.000 -0.000,33.657 -0.000,32.000 C-0.000,30.343 1.343,29.000 3.000,29.000 C4.657,29.000 6.000,30.343 6.000,32.000 C6.000,33.657 4.657,35.000 3.000,35.000 ZM3.000,21.000 C1.343,21.000 -0.000,19.657 -0.000,18.000 C-0.000,16.343 1.343,15.000 3.000,15.000 C4.657,15.000 6.000,16.343 6.000,18.000 C6.000,19.657 4.657,21.000 3.000,21.000 ZM3.000,6.000 C1.343,6.000 -0.000,4.657 -0.000,3.000 C-0.000,1.343 1.343,-0.000 3.000,-0.000 C4.657,-0.000 6.000,1.343 6.000,3.000 C6.000,4.657 4.657,6.000 3.000,6.000 ZM15.000,29.000 C16.657,29.000 18.000,30.343 18.000,32.000 C18.000,33.657 16.657,35.000 15.000,35.000 C13.343,35.000 12.000,33.657 12.000,32.000 C12.000,30.343 13.343,29.000 15.000,29.000 Z"/>
-                                        </svg> 
-                                    </div> 
-                                </td>
-                                <!-- <td> <img :src="'/' + t.assignee[0].image_url"> </td> -->
-                                <td class="assignee"> <img src="/img/temporary/user1.png"> </td>
-                                <td class="projects"> 
-                                    <div class="buzz-overflow task-project"> {{ t.title }} </div>
-                                    <span class="assigned-project"> assigned to {{
-                                        t.assigned == null ? '' :
-                                        t.assigned[0].first_name + ' ' + t.assigned[0].last_name 
-                                        }}. {{ t.created_at | momentAgo}} </span>
-                                </td>
-                                <td> 
-                                <span class="status"> {{ t.status }} </span>
-                                <div class="progress" :class="t.status"> </div>
-                                </td>
-                            </tr>
-                            <tr class="box-footer">
-                                <a href="#"> View More </a>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
@@ -141,6 +114,9 @@
                         </tbody>
                     </table>
                 </div>
+            </div>
+            <div class="box-footer">
+                <a href="#"> View More </a>
             </div>
         </div>
     </div>
