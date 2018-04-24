0807604e1d5338fae0cb10bab208f145cb2730d4
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-04-24 17:17:02 +0800

    Added client details

---
+++ resources/assets/js/components/clients/Details.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class="content clients">
+        <div class="content-header">
+             <div class="row">
+                <div class="col-md-6">
+                    <div class="page-title">
+                        <h1> <span class="prev-path"> Dashboard </span> 
+                            &nbsp; <img src="/img/icons/ArrowRight.svg"> 
+                            &nbsp; <span class="prev-path"> Clients </span>
+                            &nbsp; <img src="/img/icons/ArrowRight.svg"> 
+                            &nbsp; <span class="current">{{ full_name }}</span>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-body">
+            <section class="buzz-section">
+                <profile></profile>
+            </section>
+        </div>
+    </section>
+</template>
+
+<script>
+    import Profile from './profile/Index.vue';
+
+    export default {   
+        components: {
+          'profile': Profile,
+        },
+       data () {
+        return {
+            full_name: ''
+        }
+      },
+
+      mounted () {
+      },
+        methods: {
+        }
+    }
+</script>
