72661f4112173061ca5ac0d1009e2824007df629
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-05-10 23:41:18 +0800

    Fix concat

--- app/Company.php
+++ app/Company.php
@@ -103,7 +103,12 @@ class Company extends Model
 
     public function allCompanyMembers()
     {
-        return $this->members()->get();
+        return $this->members()
+                    ->select(
+                        'users.id',
+                        DB::raw('CONCAT(CONCAT(UCASE(LEFT(users.last_name, 1)), SUBSTRING(users.last_name, 2)), ", ", CONCAT(UCASE(LEFT(users.first_name, 1)), SUBSTRING(users.first_name, 2))) AS name')
+                    )->get();
+                    
     }
 
     public function paginatedCompanyMembers(Request $request)
