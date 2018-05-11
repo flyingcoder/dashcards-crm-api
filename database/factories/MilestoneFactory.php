a11a52c54f26b6d9d621d58537d7419598d1b9e9
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-05-11 21:30:59 +0800

    Fix problem with the seeder

--- database/factories/MilestoneFactory.php
+++ database/factories/MilestoneFactory.php
@@ -8,7 +8,6 @@ $factory->define(App\Milestone::class, function (Faker $faker) {
        		return factory(App\Project::class)->create()->id;
        	},
        	'title' => $faker->realText($maxNbChars = 50, $indexSize = 1),
-       	'percentage' => 1,
        	'status' => $faker->randomElement(['in progress', 'done', 'late']),
        	'started_at' => $faker->date($format = 'Y-m-d', $max = 'now'),
        	'end_at' => $faker->date($format = 'Y-m-d', $max = 'now')
