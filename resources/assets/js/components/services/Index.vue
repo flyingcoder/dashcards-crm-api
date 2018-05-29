9a7d67dc39ae8f570b93fe3a717d49258bbe28e6
Klent Gary <klent.buzzooka@gmail.com>
2018-05-29 16:16:50 +0800

    Fix Modal Form and Group Page Design

--- resources/assets/js/components/services/Index.vue
+++ resources/assets/js/components/services/Index.vue
@@ -3,7 +3,11 @@
         <v-layout row wrap>
              <div class="col-md-6 content-header">
                 <div class="page-title">
-                    <h1> <span class="prev-path"> Dashboard </span> &nbsp; <img src="img/icons/ArrowRight.svg"> &nbsp; <span class="current"> Services </span> </h1>
+                    <h1> 
+                        <span class="prev-path"> Dashboard </span> 
+                        &nbsp; <img src="img/icons/ArrowRight.svg"> &nbsp; 
+                        <span class="current"> Services </span> 
+                    </h1>
                 </div>
             </div>
             <div class="col-md-6">
@@ -36,7 +40,7 @@
                                 </el-dropdown-menu>  
                             </el-dropdown>
                         </li>
-                        <li>
+                        <!-- <li>
                             <a href="#grid-view" data-toggle="tab">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                                     width="18px" height="17px">
@@ -44,7 +48,7 @@
                                     d="M17.324,16.511 C16.889,16.940 16.183,16.940 15.748,16.511 L9.191,10.039 L2.635,16.511 C2.200,16.940 1.494,16.940 1.058,16.511 C0.623,16.080 0.623,15.384 1.058,14.954 L7.614,8.484 L1.058,2.013 C0.623,1.583 0.623,0.886 1.058,0.456 C1.494,0.026 2.200,0.026 2.635,0.456 L9.191,6.927 L15.748,0.456 C16.183,0.026 16.889,0.026 17.324,0.456 C17.759,0.886 17.759,1.583 17.324,2.013 L10.768,8.484 L17.324,14.954 C17.759,15.384 17.759,16.080 17.324,16.511 Z"/>
                                 </svg>
                             </a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
