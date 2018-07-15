48ba39ac0157c7845812b7feb52b3f9d84b96766
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-07-16 03:28:43 +0800

    Added the api login controller

--- app/Http/Controllers/Auth/ApiLoginController.php
+++ app/Http/Controllers/Auth/ApiLoginController.php
@@ -41,11 +41,14 @@ class ApiLoginController extends Controller
      * 
      * @return \Illuminate\Http\Response 
      */ 
-    public function login(){ 
+    public function login(){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')->accessToken; 
-            return response()->json(['success' => $success], $this->successStatus); 
+            return response()->json([
+                'success' => $success, 
+                'user' => $user 
+            ], $this->successStatus); 
         } 
         else{ 
             return response()->json(['error'=>'Unauthorised'], 401); 
