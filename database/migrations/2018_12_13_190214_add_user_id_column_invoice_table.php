bfa61b93395c26d9257ec26ca966c63fac40b667
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-12-18 02:20:08 +0800

    Fix migration error

--- database/migrations/2018_12_13_190214_add_user_id_column_invoice_table.php
+++ database/migrations/2018_12_13_190214_add_user_id_column_invoice_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnInvoiceTable extends Migration
+class AddUserIdColumnInvoiceTable extends Migration
 {
     /**
      * Run the migrations.
