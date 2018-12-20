2afb979886d6ca95bd31fad91e7f40fc557508ef
alvin_buzzooka <alvin.buzzooka@gmail.com>
2018-12-20 23:33:54 +0800

    Update invoice test

--- tests/Feature/InvoiceTest.php
+++ tests/Feature/InvoiceTest.php
@@ -16,9 +16,14 @@ class InvoiceTest extends TestCase
 
         $user = User::all()->first();
 
+        $data = [
+            'title' => 'THis is my invoice',
+            'terms' => 'adsfs'
+        ];
+
         $response = $this->actingAs($user, 'api')
                          ->withHeaders(['HTTP_X-Requested-With' => 'XMLHttpRequest'])
-                         ->get('api/invoice');
+                         ->post('api/invoice', $data);
 
         dd($response->content());
         $response->assertStatus(200);
