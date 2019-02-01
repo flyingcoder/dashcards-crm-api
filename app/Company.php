7be4d0c5f893a9260196dcc29ddd08fb95a88603
alvin_buzzooka <alvin.buzzooka@gmail.com>
2019-02-02 06:09:34 +0800

    changing data vars on teams

--- app/Company.php
+++ app/Company.php
@@ -402,8 +402,10 @@ class Company extends Model
         $data = $members->paginate($this->paginate);
 
         $data->map(function ($user) {
-            $user['tasks_count'] = $user->tasks()->count();
-            $user['project_count'] = $user->projects()->count();
+            unset($user['tasks']);
+            unset($user['projects']);
+            $user['tasks'] = $user->tasks()->count();
+            $user['projects'] = $user->projects()->count();
             $roles = $user->roles()->first();
             if(!is_null($roles))
                 $user['group_name'] = $roles->id;
