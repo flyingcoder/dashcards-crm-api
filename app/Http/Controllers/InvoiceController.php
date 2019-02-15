5c74c646e68324f1588eb088bca8ef07fac7023d
alvin_buzzooka <alvin.buzzooka@gmail.com>
2019-02-16 04:18:20 +0800

    asdfas

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -57,7 +57,7 @@ class InvoiceController extends Controller
         $invoice->due_date = request()->due_date;
         $invoice->title = request()->title;
         $invoice->total_amount = request()->total_amount;
-        $invoice->items = json_encode(request()->items);
+        $invoice->items = json_decode(request()->items)[0];
         $invoice->type = request()->type;
 
         if(request()->has('project_id'))
