f3cd98ab5ba40f70c0eed41782a4fc19e5a9b4e1
alvin_buzzooka <alvin.buzzooka@gmail.com>
2019-03-26 22:36:15 +0800

    fix schema issue

--- app/Company.php
+++ app/Company.php
@@ -379,7 +379,7 @@ class Company extends Model
 
         $model = $team->members()
                       ->select(
-                        'users.*'
+                        'users.*',
                         'users.is_online',
                         DB::raw('CONCAT(CONCAT(UCASE(LEFT(users.last_name, 1)), SUBSTRING(users.last_name, 2)), ", ", CONCAT(UCASE(LEFT(users.first_name, 1)), SUBSTRING(users.first_name, 2))) AS name')
                       )->orderBy('users.created_at', 'DESC');
