6993e849d744b522a5f91f5b5a21649617c01016
alvin_buzzooka <alvin.buzzooka@gmail.com>
2019-04-26 00:49:32 +0800

    created new migration

--- app/Form.php
+++ app/Form.php
@@ -47,7 +47,7 @@ class Form extends Model
 
     public function service()
     {
-        return $this->belongsToMany(Service::class)
+        return $this->belongsToMany(Service::class);
     }
 
     public function user()
