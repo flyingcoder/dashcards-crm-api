17e4da620813e0e28897c01ed535c661e62b9bb1
alvin_buzzooka <alvin.buzzooka@gmail.com>
2020-02-01 05:56:52 +0800

    Added counter data on paginator

--- app/Task.php
+++ app/Task.php
@@ -26,6 +26,21 @@ class Task extends Model
 
     protected static $logName = 'system';
 
+    public function toArray() {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'milestone_id' => $this->milestone_id,
+            'status' => $this->status,
+            'days' => $this->days,
+            'total_time' => $this->total_time(),
+            'assignee' => $this->assigned,
+            'description' => $this->description,
+            'started_at' => $this->started_at,
+            'end_at' => $this->end_at
+        ];
+    }
+    
     public function getDescriptionForEvent(string $eventName): string
     {
         return "A task has been {$eventName}";
