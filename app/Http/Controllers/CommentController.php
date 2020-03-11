2d77969cc2659aea6131802ad8a53104b06b00eb
kirbycaps <kcgerona@gmail.com>
2020-03-12 01:54:18 +0800

    added api function for deleting comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    
+    public function delete($id)
+    {
+    	$comment = Comment::findOrFail($id);
+    	//todo add layer for checking of capabilities to delete a comment
+
+    	if($comment->delete()){
+    		return response()->json([
+    			'comment_id' => $id,
+    			'message' => 'Successfully deleted'
+    		], 200);
+    	}
+    	return response()->json([
+    		'message' => "Can't delete comment."
+    	], 522);
+    }
 }
