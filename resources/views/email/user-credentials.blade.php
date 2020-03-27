e3cc222bc8e10d25b6d1b2744d6684b933c2cca2
kirbycaps <kcgerona@gmail.com>
2020-03-28 04:21:40 +0800

    Added email templates

--- resources/views/email/user-credentials.blade.php
+++ resources/views/email/user-credentials.blade.php
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Welcome to Buzzooka CRM </h1>
 
-	<h5>Hello, {{ $user->first_name }}</h5>
-	<p>
-		Your username is "{{ $user->username }}", <br>
-		You can set your password by following this link <a href="{{$link}}">{{ $link }}</a>
-	</p>
-</body>
-</html>
+@extends('email.master')
+
+@section('title', '')
+
+@section('content')
+   <!-- START CENTERED WHITE CONTAINER -->
+    <table role="presentation" class="main">
+
+      <!-- START MAIN CONTENT AREA -->
+      <tr>
+        <td class="wrapper">
+          <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+              <td>
+                <p>Hello, {{ $user->first_name }}</p>
+                <p>Your username is "{{ $user->username }}",</p>
+                <p>Your password is "{{ $password }}".</p>
+                <p>You can login here <a href="{{$login_link}}" target="_blank">{{ $login_link }}</a></p>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+
+    <!-- END MAIN CONTENT AREA -->
+    </table>
+@endsection
---
+++ resources/views/email/user-reset-password.blade.php
@@ -0,0 +1,41 @@
+@extends('email.master')
+
+@section('title', '')
+
+@section('content')
+<!-- START CENTERED WHITE CONTAINER -->
+<table role="presentation" class="main">
+
+  <!-- START MAIN CONTENT AREA -->
+  <tr>
+    <td class="wrapper">
+      <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td>
+            <p>Hello, {{ $user->first_name }}</p>
+            <p>Your username is "{{ $user->username }}", <br>To set your password follow below link</p>
+
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+              <tbody>
+                <tr>
+                  <td align="left">
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                      <tbody>
+                        <tr>
+                          <td><a href="{{$link}}" target="_blank">{{ $link }}</a></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+
+<!-- END MAIN CONTENT AREA -->
+</table>
+@endsection
