3889d243490b8d4a1c3c1896eefb54eff92e9920
kirbycaps <kcgerona@gmail.com>
2020-06-12 18:59:08 +0800

    Added 1 default template

---
+++ database/seeds/DefaultInvoiceTemplates.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DefaultInvoiceTemplates extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $template =  \App\Template::create([
+        		'name' => 'Default-1',
+        		'company_id' => 0,
+        		'status' => 'active',
+        		'replica_type' => 'App\\Invoice',
+        		'created_at' => now()->format('Y-m-d H:i:s')
+	    	]);
+        
+        $html  = view('invoices.template-1')->render();
+
+        $template->setMeta('template', $html);
+    }
+}
